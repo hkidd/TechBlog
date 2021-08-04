c5d73f926dabf1a65c1abc86bae72039d2e6f9a0
hkidd <harrisonakidd@gmail.com>
2021-08-03 22:23:15 -0500

    Added JWS to connection

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
-//   cookie: {},
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
